3e44484758494df075503449047e39068d4f0454
Jiayi Pan <i@jiayipan.me>
2023-07-27 00:23:33 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 subscribers = ["jiayidotpan@gmail.com"] # list of subscribers to send emails to
-domains = ["cs.AI", "cs.CV", "cv.CL"] # list of research domains to watch, check https://arxiv.org/category_taxonomy for more info
+domains = ["cs.AI", "cs.CV", "cs.CL"] # list of research domains to watch, check https://arxiv.org/category_taxonomy for more info
 resend_key = None # You can either set the resend email API key in the environment variable or right here 
 num_emails = 3 # number of emails to send, each email contains 1/num_emails of the papers
 
